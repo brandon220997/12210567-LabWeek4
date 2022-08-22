a6b9f1e43162445907524d5fa581c53dd89b9aa5
Brandon Cramer <brandon.cramer1997@gmail.com>
2022-08-22 19:46:57 +1000

    100 Percent Complete

--- Assets/PrintAndHide.cs
+++ Assets/PrintAndHide.cs
@@ -6,16 +6,29 @@ public class PrintAndHide : MonoBehaviour
 {
     public Renderer rend;
     private int i;
+    private int blueDisable;
 
     // Start is called before the first frame update
     void Start()
     {
         i = 3;
+        blueDisable = UnityEngine.Random.Range(200, 251);
+        if (transform.tag == "Blue") Debug.LogWarning($"Blue Disable: {blueDisable}");
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         Debug.Log($"{transform.name}:{++i}");
+        
+        if (transform.tag == "Red" && i == 100)
+        {
+            transform.gameObject.SetActive(false);
+        }
+        if (transform.tag == "Blue" && i == blueDisable)
+        {
+            transform.GetComponent<Renderer>().enabled = false;
+        }
     }
 }
